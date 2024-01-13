79309cd03ce8749d6119f80782dab2a7e0ce8189
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-01-12 21:05:46 -0300

    Reorganizando stack

--- web/chat-web/src/App.js
+++ web/chat-web/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Login from './pages/Login/Login.jsx';
 import Chat from './pages/Chat/Chat.jsx';
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Chat />} />
+				<Route path="/" element={<Login />} />
+				<Route path="/chat" element={<Chat />} />
 			</Routes>
 		</Router>
 	);
