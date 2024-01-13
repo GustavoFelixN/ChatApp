09af1b675113eab87a80461c50120868e4751ec0
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-01-13 20:56:54 -0300

    Recebendo dados na pagina de Chat

--- web/chat-web/src/pages/Chat/Chat.jsx
+++ web/chat-web/src/pages/Chat/Chat.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Chat = () => {
+	const location = useLocation();
+	const [, setName] = useState('');
+	const [, setRoom] = useState('');
+
+	useEffect(() => {
+		const { name, room } = location.state;
+		setName(name);
+		setRoom(room);
+	}, [location])
+
 	return (
 		<div>
-			<h1>W's in the Chat</h1>
+			<h1>Chat</h1>
 		</div>
 	);
 }
