405c911de49418c8ced9138afee4b78b31cbcf6b
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-01-13 14:49:44 -0300

    Mudando estilo do Input

--- web/chat-web/src/components/Input/styles.js
+++ web/chat-web/src/components/Input/styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { Input } from 'antd';
 
 export const BaseInput = styled(Input)`
-	margin-bottom: 10px;
 	background-color: #380356;
 	color: #fff;
 	&::placeholder {
