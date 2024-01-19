f73ccdd73ad59414404fe3d4fbadbe3bf0d3000e
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-01-19 00:05:49 -0300

    Exportando novo componente

--- web/chat-web/src/components/index.js
+++ web/chat-web/src/components/index.js
@@ -1,4 +1,5 @@
 import Input from './Input/Input.jsx';
 import Button from './Button/Button.jsx';
+import Message from './Message/Message.jsx';
 
-export { Input, Button };
+export { Input, Button, Message };
