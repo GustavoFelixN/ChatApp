ff9cb471c2ec5d7be9c9f34cd88b19516e45c25d
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-01-19 02:24:24 -0300

    Implementando comunicação entre input e componente pai

--- web/chat-web/src/components/MessageInput/MessageInput.jsx
+++ web/chat-web/src/components/MessageInput/MessageInput.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Space } from 'antd';
 import { Input, Button } from '../index.js';
 
-const MessageInput = () => {
+const MessageInput = ({ submitCallback }) => {
+	const [message, setMessage] = useState('');
+	const handleSubmit = () => {
+		submitCallback(message, () => setMessage(''));
+	};
+
 	return (
 		<Space.Compact style={{ width: '100%' }}>
-			<Input size="large" placeholder="Envie uma mensagem.."/>
-			<Button size="large" type="submit">Enviar</Button>
+			<Input
+				size="large"
+				placeholder="Envie uma mensagem.."
+				value={message}
+				onChange={(e) => setMessage(e.target.value)}
+			/>
+			<Button size="large" htmlType="submit" onClick={handleSubmit}>Enviar</Button>
 		</Space.Compact>
 	);
 };
